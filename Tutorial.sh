c10ed5cf6e1a33a28dcf5bcc03c2af0e043203e0
acosi <cosianthony@gmail.com>
2025-01-24 02:20:32 +0100

    wordpress started

--- Tutorial.sh
+++ Tutorial.sh
@@ -1,5 +1,9 @@
 # Install docker and other dependencies :
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin openssl
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin openssl python3-dev python3-distutils
+
+# Adds the user to the docker group, allowing to use it without sudo
+# (requires logout to take effect)
+sudo usermod -aG docker $USER
 
 # Download and test the docker image for debian:buster and apline 3.21
 sudo docker run -it debian:buster bash
@@ -11,7 +15,14 @@ docker build -t image-name .
 # Rune a container from the specified image, opening the specified port
 docker run -p 443:443 --name container-name image-name
 
+# Access the container's shell
+docker exec -it container-name bash
+
+# Check mariadb
+mysql -u root -p
+SHOW DATABASES;
+
 # Remove a docker and its image
-docker container-name
-docker docker rm container-name
+docker stop container-name
+docker rm container-name
 docker rmi container-image
--- srcs/requirements/mariadb/configure.sh
+++ srcs/requirements/mariadb/configure.sh
@@ -1,39 +1,20 @@
 #!/bin/bash
 
-# Step 1: Verify and create /run/mysqld if it doesn't exist
-if [ ! -d /run/mysqld ]; then
-    echo \"Creating /run/mysqld directory...\"
-    mkdir -p /run/mysqld
-    chown -R mysql:mysql /run/mysqld
-fi
+mkdir -p /run/mysqld && chown mysql:mysql /run/mysqld
 
-# Step 2: Verify and initialize the database directory if it doesn't exist
-if [ ! -d /var/lib/mysql/mysql ]; then
-    echo \"Initializing MariaDB database...\"
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
-    
-    echo \"Running mysql_secure_installation...\"
-    mysql_secure_installation <<EOF
+mysqld_safe &
 
-y
-password123
-password123
-y
-y
-y
-y
+sleep 10
+
+mariadb -u root <<EOF
+CREATE DATABASE $SQL_DATABASE;
+CREATE USER $SQL_USER@'%' IDENTIFIED BY '$SQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO $SQL_USER@'%';
+FLUSH PRIVILEGES;
 EOF
-fi
 
-# Step 3: Modify the mariadb-server.cnf file
-CONF_FILE=\"/etc/mysql/mariadb.conf.d/50-server.cnf\"
-if [ -f \"$CONF_FILE\" ]; then
-    echo \"Configuring MariaDB server...\"
-    sed -i '/^skip-networking/s/^/#/' $CONF_FILE
-    sed -i 's/^bind-address.*/bind-address = 0.0.0.0/' $CONF_FILE
-fi
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';"
 
-# Step 4: Launch MariaDB in console mode
-echo \"Starting MariaDB...\"
-exec mysqld_safe --console
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
+mysqld_safe
